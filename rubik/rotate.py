bc15b238b69cae8c2a1f0d6fa1f199cc558b9a70
Hunter Westerlund <hunterw.mail@gmail.com>
2022-09-01 22:18:56 -0500

    Green Light |

--- rubik/rotate.py
+++ rubik/rotate.py
@@ -1,4 +1,5 @@
 import rubik.cube as rubik
+import re
 
 def _rotate(parms):
     """Return rotated cube""" 
@@ -11,5 +12,7 @@ def _rotate(parms):
 
 def _validate(cube):
     if len(cube) != 54:
+        return False
+    if re.search(r'^[bgorwy]*$', cube) != None:
         return False
     return True
