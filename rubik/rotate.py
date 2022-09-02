355506cec9876a3453978c1a20cb1eb043b978f5
Hunter Westerlund <hunterw.mail@gmail.com>
2022-09-02 02:07:33 -0500

    Green Light |

--- rubik/rotate.py
+++ rubik/rotate.py
@@ -12,8 +12,57 @@ def _rotate(parms):
     elif not _validateDir(encodedDir):
         result['status'] = 'error: invalid rotation'
     else:
-        rotatedCube = encodedCube                  #STUB:  rotate the cube
-        result['cube'] = 'orgowobgrowwwrwggwrbgrybgorywyroybyorobbbgbyywrwbgyygo'               
+        cubeRot = list(encodedCube)
+        
+        if dir == 'F':
+            offset = 0
+            faceRot = _faceCW(''.join(cubeRot[0:9]))
+            
+            for i, ch in enumerate(faceRot):
+                cubeRot[i + offset] = ch
+            
+            # Right Edges
+            cubeRot[47] = encodedCube[ 9]
+            cubeRot[46] = encodedCube[12]
+            cubeRot[45] = encodedCube[15]
+            # Left Edges
+            cubeRot[44] = encodedCube[29]
+            cubeRot[43] = encodedCube[32]
+            cubeRot[42] = encodedCube[35]
+            # Top Edges
+            cubeRot[ 9] = encodedCube[42]
+            cubeRot[12] = encodedCube[43]
+            cubeRot[15] = encodedCube[44]
+            # Bottom Edges
+            cubeRot[29] = encodedCube[45]
+            cubeRot[32] = encodedCube[46]
+            cubeRot[35] = encodedCube[47]
+            
+        elif dir == 'f':
+            offset = 0
+            faceRot = _faceCCW(''.join(cubeRot[0:9]))
+            
+            for i, ch in enumerate(faceRot):
+                cubeRot[i + offset] = ch
+                
+            # Right Edges
+            cubeRot[42] = encodedCube[ 9]
+            cubeRot[43] = encodedCube[12]
+            cubeRot[44] = encodedCube[15]
+            # Left Edges
+            cubeRot[45] = encodedCube[29]
+            cubeRot[46] = encodedCube[32]
+            cubeRot[47] = encodedCube[35]
+            # Top Edges
+            cubeRot[35] = encodedCube[42]
+            cubeRot[32] = encodedCube[43]
+            cubeRot[29] = encodedCube[44]
+            # Bottom Edges
+            cubeRot[15] = encodedCube[45]
+            cubeRot[12] = encodedCube[46]
+            cubeRot[ 9] = encodedCube[47]
+            
+        result['cube'] = ''.join(cubeRot)          
         result['status'] = 'ok'                     
     return result
 
