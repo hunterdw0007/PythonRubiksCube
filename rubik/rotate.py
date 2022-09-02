b6c5813ae4a8562bc389ce5210c9fe1d85938226
Hunter Westerlund <hunterw.mail@gmail.com>
2022-09-01 22:20:25 -0500

    Green Light |

--- rubik/rotate.py
+++ rubik/rotate.py
@@ -13,6 +13,6 @@ def _rotate(parms):
 def _validate(cube):
     if len(cube) != 54:
         return False
-    if re.search(r'^[bgorwy]*$', cube) != None:
+    if re.search(r'^[bgorwy]*$', cube) == None:
         return False
     return True
