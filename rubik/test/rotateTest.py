62293289a010f98f4d8f45592caf06eb6f646f44
Hunter Westerlund <hunterw.mail@gmail.com>
2022-09-05 21:42:06 -0500

    Red Light |

--- rubik/test/rotateTest.py
+++ rubik/test/rotateTest.py
@@ -140,6 +140,21 @@ class RotateTest(unittest.TestCase):
         self.assertEqual(expectResult.get('cube'), actualResult.get('cube'))
         self.assertEqual(expectResult.get('status'), actualResult.get('status'))
         
+    def test_rotate_070_shouldRotateValidCuber(self):
+        inputDict = {}
+        inputDict['op']   = 'rotate'
+        inputDict['cube'] = 'gorrwgoobwwwrrwwgwrbgrybgorywyroybybrobbbgowgyyobgyygo'
+        inputDict['dir']  = 'r'
+        
+        expectResult = {}
+        expectResult['cube'] = 'gobrwgoogwwwwrgwrwobgyyboorywyroybybrogbbrowryyrbggygb'
+        expectResult['status'] = 'ok'
+        
+        actualResult = rotate._rotate(inputDict)
+        
+        self.assertEqual(expectResult.get('cube'), actualResult.get('cube'))
+        self.assertEqual(expectResult.get('status'), actualResult.get('status'))
+        
     def test_rotate_910_missingCube(self):
         inputDict = {}
         inputDict['op']   = 'rotate'
