49a362d780a3bc9bf538a8d81084acf196cfb464
Hunter Westerlund <hunterw.mail@gmail.com>
2022-09-18 01:07:00 -0500

    Red Light |

---
+++ rubik/test/bottomCrossTest.py
@@ -0,0 +1,52 @@
+'''
+Created on Sep 18, 2022
+
+@author: Hunter
+'''
+import unittest
+import rubik.solve as solve
+
+class BottomCrossTest(unittest.TestCase):
+
+
+    def setUp(self):
+        pass
+
+
+    def tearDown(self):
+        pass
+
+# Analysis
+#
+# inputs:
+#    parms: dict: mandatory: arrives validated
+#    parms['op']: string; 'solve', mandatory, arrives validated
+#    parms['cube']: string; len=54, [browyg], 9 occurences of each character, unique middle color; mandatory; arrives unvalidated
+#
+# outputs:
+#    side-effects: no state changes; no external effects
+#    returns: dict
+#    nominal:
+#        dict['solution']: string, rotations to solve cube bottom cross
+#        dict['status']: string, 'ok'
+#    abnormal:
+#        dict['status']: string, 'error: xxx', xxx is message
+#
+#    confidence level: boundary value analysis
+#
+# happy path:
+#    test 010: solved cube
+
+    def test_bottomCross_010_solvedCube(self):
+        inputDict = {}
+        inputDict['op']   = 'solve'
+        inputDict['cube'] = 'wwwwwwwwwrrrrrrrrryyyyyyyyyooooooooobbbbbbbbbggggggggg'
+        
+        expectResult = {}
+        expectResult['solution'] = ''
+        expectResult['status'] = 'ok'
+        
+        actualResult = solve._solve(inputDict)
+        
+        self.assertEqual(expectResult.get('cube'), actualResult.get('cube'))
+        self.assertEqual(expectResult.get('status'), actualResult.get('status'))
