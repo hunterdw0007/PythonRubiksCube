b2c872310030aba000c8e4c076ab2044db570a78
Hunter Westerlund <hunterw.mail@gmail.com>
2022-09-18 13:42:27 -0500

    Green Light |

--- rubik/solve.py
+++ rubik/solve.py
@@ -3,7 +3,7 @@ import rubik.cube as rubik
 def _solve(parms):
     """Return rotates needed to solve input cube"""
     result = {}
-    encodedCube = parms.get('cube',None)       #STUB:  get "cube" parameter if present
-    result['solution'] = ''        #STUB:  example rotations
+    encodedCube = parms.get('cube',None)
+    result['solution'] = ''
     result['status'] = 'ok'                     
     return result
