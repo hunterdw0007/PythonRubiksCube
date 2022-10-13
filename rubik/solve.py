04bff7b94d53ab940ed8de416090f8b72ab9113e
Hunter Westerlund <hunterw.mail@gmail.com>
2022-10-13 14:01:34 -0500

    Green Light |

--- rubik/solve.py
+++ rubik/solve.py
@@ -447,8 +447,26 @@ def _moveWrongBottomCornerToTop(cube):
             break
     
     return cube, location, rotations
-        
-        
+
+def _checkMiddleLayer(cube):
+    # Checks whether or not the middle edges are solved independent of any other part of the cube being solved
+    if cube[rotate.cubeEnum.F10.value] != cube[rotate.cubeEnum.F11.value]:
+        return False
+    if cube[rotate.cubeEnum.F12.value] != cube[rotate.cubeEnum.F11.value]:
+        return False
+    if cube[rotate.cubeEnum.R10.value] != cube[rotate.cubeEnum.R11.value]:
+        return False
+    if cube[rotate.cubeEnum.R12.value] != cube[rotate.cubeEnum.R11.value]:
+        return False
+    if cube[rotate.cubeEnum.B10.value] != cube[rotate.cubeEnum.B11.value]:
+        return False
+    if cube[rotate.cubeEnum.B12.value] != cube[rotate.cubeEnum.B11.value]:
+        return False
+    if cube[rotate.cubeEnum.L10.value] != cube[rotate.cubeEnum.L11.value]:
+        return False
+    if cube[rotate.cubeEnum.L12.value] != cube[rotate.cubeEnum.L11.value]:
+        return False
+    return True
         
         
         
