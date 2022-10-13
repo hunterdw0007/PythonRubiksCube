376b1d458af0a106ef33959f27c264097f01ad2d
Hunter Westerlund <hunterw.mail@gmail.com>
2022-10-13 14:42:30 -0500

    Red Light |

--- rubik/test/middleLayerTest.py
+++ rubik/test/middleLayerTest.py
@@ -27,7 +27,7 @@ class Test(unittest.TestCase):
 #    test 020: solved bottom not middle
 #    test 030: solved middle, scrambled elsewhere
 
-    def test_middleLayer_010_solvedMiddleEdgesOfSolvedCubeCheck(self):
+    def test_checkMiddleLayer_010_solvedMiddleEdgesOfSolvedCubeCheck(self):
         cube = 'wwwwwwwwwrrrrrrrrryyyyyyyyyooooooooobbbbbbbbbggggggggg'
         
         expectResult = True
@@ -36,7 +36,7 @@ class Test(unittest.TestCase):
         
         self.assertEqual(expectResult, actualResult)
     
-    def test_middleLayer_020_solvedMiddleEdgesOfSolvedBottomCheck(self):
+    def test_checkMiddleLayer_020_solvedMiddleEdgesOfSolvedBottomCheck(self):
         cube = 'byybwbwwwbyrrrorrrboobyryyywoowowooobwwybrybrggggggggg'
         
         #checking that cube is valid
@@ -48,7 +48,7 @@ class Test(unittest.TestCase):
         
         self.assertEqual(expectResult, actualResult)
 
-    def test_middleLayer_030_solvedMiddleEdgesOnly(self):
+    def test_checkMiddleLayer_030_solvedMiddleEdgesOnly(self):
         cube = 'rgbbbbyybyborrrywrwgogggwrgywgoooowrgwbbyywyogorowrwyb'
         
         #checking that cube is valid
@@ -59,3 +59,38 @@ class Test(unittest.TestCase):
         actualResult = solveMiddleLayer._checkMiddleLayer(cube)
         
         self.assertEqual(expectResult, actualResult)
+
+# Analysis - solve._locateMiddlePieceInTop
+#
+# inputs:
+#    cube: string; len=54, [browyg], 9 occurences of each character, unique middle color; mandatory; arrives validated
+#
+# outputs:
+#    side-effects: no state changes; no external effects
+#    returns: integer, 0-54 corresponding to location
+#
+#    confidence level: boundary value analysis
+#
+# happy path:
+#    test 010: edge found in Front
+#    test 020: edge found elsewhere
+# 
+# sad path:
+#    test 910: no edge found
+
+    def test_locateMiddlePieceInTop_010_edgeFoundInFront(self):
+        cube = 'gogobbbbbogyyrrrrrbybygbgggyrrooyoooogrbyrygywwwwwwwww'
+        
+        #checking that cube is valid
+        self.assertEqual(verify._validateCube(cube), True)
+        
+        expectResult = rotate.cubeEnum.F01.value
+        
+        actualResult = solveMiddleLayer._locateMiddlePieceInTop(cube)
+        
+        self.assertEqual(expectResult, actualResult)
+        
+        
+        
+        
+            
