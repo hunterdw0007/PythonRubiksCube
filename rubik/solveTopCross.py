79fdc39f444436e622a44989f9295f822f2ae114
Hunter Westerlund <hunterw.mail@gmail.com>
2022-10-24 17:01:19 -0500

    Green Light |

--- rubik/solveTopCross.py
+++ rubik/solveTopCross.py
@@ -31,10 +31,17 @@ def _checkCrossState( cube ):
     topEdges = [ cube[rotate.cubeEnum.U01.value], cube[rotate.cubeEnum.U10.value]
                , cube[rotate.cubeEnum.U12.value], cube[rotate.cubeEnum.U21.value]]
     
-    # Case 1: Cross created
-    if topEdges.count(topEdges[0]) == len(topEdges) or topEdges[1] == topEdges[2]:
+    # Case 1: Cross, horizontal line, L in position
+    if topEdges.count(topEdges[0]) == len(topEdges) or topEdges[1] == topEdges[2] or topEdges[0] == topEdges[1]:
         return 0
-    
-    #Case 2: line created
-    if topEdges[0] == topEdges[3]:
+    # Case 2: line vertical, L in bottom left
+    if topEdges[0] == topEdges[3] or topEdges[1] == topEdges[3]:
         return 1
+    #Case 3: L in bottom right
+    if topEdges[2] == topEdges[3]:
+        return 2
+    # Case 4: L in top right
+    if topEdges[0] == topEdges[2]:
+        return 3
+    # Case 5: No edges
+    return 0
