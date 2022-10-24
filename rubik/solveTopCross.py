9e6dac54de96595f2a0604b43e38bd2bfa95edd0
Hunter Westerlund <hunterw.mail@gmail.com>
2022-10-24 16:18:36 -0500

    Green Light |

--- rubik/solveTopCross.py
+++ rubik/solveTopCross.py
@@ -7,4 +7,21 @@ Created on Oct 24, 2022
 import rubik.rotate as rotate
 
 def _checkTopCross( cube ):
+    # Returns true if the top cross is solved, including edges being on the correct side
+    if cube[rotate.cubeEnum.U01.value] != cube[rotate.cubeEnum.U11.value]:
+        return False
+    if cube[rotate.cubeEnum.U10.value] != cube[rotate.cubeEnum.U11.value]:
+        return False
+    if cube[rotate.cubeEnum.U12.value] != cube[rotate.cubeEnum.U11.value]:
+        return False
+    if cube[rotate.cubeEnum.U21.value] != cube[rotate.cubeEnum.U11.value]:
+        return False
+    if cube[rotate.cubeEnum.F01.value] != cube[rotate.cubeEnum.F11.value]:
+        return False
+    if cube[rotate.cubeEnum.R01.value] != cube[rotate.cubeEnum.R11.value]:
+        return False
+    if cube[rotate.cubeEnum.R01.value] != cube[rotate.cubeEnum.R11.value]:
+        return False
+    if cube[rotate.cubeEnum.R01.value] != cube[rotate.cubeEnum.R11.value]:
+        return False
     return True
