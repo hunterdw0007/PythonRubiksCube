5d5d94ce50165fe1a754cc64a5daec8193451b52
Hunter Westerlund <hunterw.mail@gmail.com>
2022-11-07 14:48:18 -0600

    Red Light |

---
+++ rubik/test/topLayerTest.py
@@ -0,0 +1,38 @@
+'''
+Created on Nov 6, 2022
+
+@author: Hunter
+'''
+import unittest
+import rubik.verify as verify
+import rubik.solve as solve
+
+
+class TopLayerTest(unittest.TestCase):
+
+# Analysis - solveTopLayer._checkTopLayer
+#
+# inputs:
+#    cube: string; len=54, [browyg], 9 occurences of each character, unique middle color; mandatory; arrives validated
+#
+# outputs:
+#    side-effects: no state changes; no external effects
+#    returns: boolean
+#
+#    confidence level: boundary value analysis
+#
+# happy path:
+#    test 010: fully solved cube
+#    test 020: scrambled cube
+#    test 030: scrambled cube except top
+    def test_checktopLayer_010_solvedCube(self):
+        cube = 'wwwwwwwwwrrrrrrrrryyyyyyyyyooooooooobbbbbbbbbggggggggg'
+        
+        #checking that cube is valid
+        self.assertTrue(verify._validateCube(cube))
+        
+        expectResult = True
+        
+        actualResult = solveTopLayer._checkTopLayer(cube)
+        
+        self.assertEqual(expectResult, actualResult)
