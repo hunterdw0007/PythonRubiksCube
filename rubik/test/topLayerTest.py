bb893e132e552bf00c4835a00d00622eabeabbaa
Hunter Westerlund <hunterw.mail@gmail.com>
2022-11-12 13:36:13 -0600

    Red Light |

--- rubik/test/topLayerTest.py
+++ rubik/test/topLayerTest.py
@@ -424,3 +424,17 @@ class TopLayerTest(unittest.TestCase):
         
         self.assertEqual(expectedCube, actualCube)
         self.assertEqual(expectedRotations, actualRotations)
+        
+    def test_positionTopEdges_020_barOnBack(self):
+        cube = 'brbbbbbbbobooooooogggggggggrorrrrrrrwwwwwwwwwyyyyyyyyy'
+    
+        #checking that cube is valid
+        self.assertEqual(verify._validateCube(cube), True)
+        
+        expectedCube = 'bbbbbbbbbooooooooogggggggggrrrrrrrrrwwwwwwwwwyyyyyyyyy'
+        expectedRotations = 'RuRURURuruRRRuRURURuruRR'
+        
+        actualCube, actualRotations = solveTopLayer._positionTopEdges(cube, '')
+        
+        self.assertEqual(expectedCube, actualCube)
+        self.assertEqual(expectedRotations, actualRotations)
